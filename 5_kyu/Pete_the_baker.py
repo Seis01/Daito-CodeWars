6fa54aa7a1a7d23aeb31329e31a4cad77f5e8875
Seis01 <154533385+Seis01@users.noreply.github.com>
2023-12-21 16:53:22 +0100

    solution Pete_the_baker.py

--- 5_kyu/Pete_the_baker.py
+++ 5_kyu/Pete_the_baker.py
@@ -38,7 +38,33 @@ def cakes(recipe, available):
 
 '''
 # Solution:
+def cakes(recipe, available):
+    # Initialize a list to store the maximum number of cakes for each ingredient
+    max_cakes = []
+
+    # Iterate through each ingredient in the recipe
+    for ingredient, quantity in recipe.items():
+        # Check if the ingredient exists in the available ingredients
+        if ingredient in available:
+            # Calculate the maximum number of cakes for this ingredient
+            max_cakes.append(available[ingredient] // quantity)
+        else:
+            # If the ingredient is not available, return 0 cakes
+            return 0
+
+    # Return the minimum of the maximums as the result
+    return min(max_cakes)
+
+# Tsetcase:
+print(cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}))  # 2
+print(cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000}))  # 0
 
 # Description:
 '''
+To solve this problem, we need to calculate how many cakes can be made using the available ingredients and the recipe. The idea is 
+to iterate through each ingredient in the recipe, check if it exists in the available ingredients, and calculate the maximum number 
+of cakes that can be made for each ingredient. The minimum of these maximums will be the maximum number of cakes that can be made.
+Now, we can use this function to calculate the maximum number of cakes Pete can bake given the recipe and available ingredients. 
+It returns the integer representing the maximum number of cakes.
+
 '''
